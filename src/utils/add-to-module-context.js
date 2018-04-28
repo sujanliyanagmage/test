e53f43070f805271a8f8ab16e2dac319b048e204
Sujan Liyanagamage <sujanl@zone24x7.lk>
2018-04-28 19:49:46 +0530

    deleted

--- src/utils/add-to-module-context.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/**
- * Gets a context class holding data for the planned modifications.
- */
-class AddToModuleContext {
-}
-exports.AddToModuleContext = AddToModuleContext;
-//# sourceMappingURL=add-to-module-context.js.map
